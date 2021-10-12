bcfcce544185308917d395c8efd315870583830e
gou5met <bgj0127@gmail.com>
2021-10-12 19:39:17 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:todo_jeweler/ui/view/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xffEB98FF),
+        primarySwatch: Colors.purple,
+      ),
+      home: Hello(),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  const Hello({Key? key}) : super(key: key);
+
+  Widget _mainScaffold() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'todo-jeweler',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      drawer: Drawer(
+        child: Center(
+          child: Text('hambaga'),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'NOTHING',
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: 'story'),
+          BottomNavigationBarItem(icon: Icon(Icons.input), label: 'easy input'),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Login();
+  }
+}
---
+++ lib/ui/view/login.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Image.asset(
+              'assets/images/login_background.png',
+            ),
+          ),
+          SizedBox(height: 40),
+          Text(
+            '로그인',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 34),
+          ),
+          SizedBox(height: 70),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 60),
+            child: TextField(),
+          ),
+          SizedBox(height: 40),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 60),
+            child: TextField(),
+          ),
+          SizedBox(height: 150),
+          Container(
+            width: 280,
+            height: 45,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xffEB98FF),
+              ),
+              onPressed: () {},
+              child: Text(
+                '로그인',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            width: 280,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('아직 계정이 없으신가요? 회원가입'),
+                Text('비밀번호가 기억이 나지 않아요.'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
